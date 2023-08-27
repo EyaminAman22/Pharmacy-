9a40de68790b3a4561721543c912a10dd690cff1
JFerdouss <78402103+JFerdouss@users.noreply.github.com>
2023-08-27 14:47:01 +0600

    customer skincare

--- Cu-Skincare.php
+++ Cu-Skincare.php
@@ -1,4 +1,10 @@
-
+<?php
+    session_start();
+    include('db_connect.php');
+    $sql = "SELECT* FROM medicine WHERE category LIKE 'skincare'";
+    $result = mysqli_query($conn,$sql);
+    
+?>
 <!doctype html>
 <html>
 <head>
@@ -20,29 +26,29 @@
             <i class="fa-solid fa-user text-white text-[100px]"></i>
             <nav class="">
                 <ul class="flex pt-10 flex-col text-20">
-                    <a href="">
+                    <a href="CustomerDashboard.php">
                         <li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400"> Dash Board</li>
                     </a>
-                    <a href="">
+                    <a href="Cu-medicine.php">
                         <li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Medicine</li>
                     </a>
-                    <a href="">
+                    <a href="Cu-surgicalEquipment.php">
                         <li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Surgical Equipment
                         </li>
                     </a>
-                    <a href="">
+                    <a href="Cu-healthcare.php">
                         <li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Health Care
                             products</li>
                     </a>
-                    <a href="">
+                    <a href="Cu-Skincare.php">
                         <li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Skin Care Products
                         </li>
                     </a>
-                    <a href="">
+                    <a href="Cu-babycare.php">
                         <li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Baby Care Products
                         </li>
                     </a>
-                    <a href="">
+                    <a href="Cu-Supplyment&Vitamin.php">
                         <li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Food Supplyment and
                             Vitamins</li>
                     </a>
@@ -56,21 +62,27 @@
                 <input type="text" class="w-96 rounded-xl h-[30px] font-normal ml-[-80px]"
                     placeholder="    Search Here">
                 <h1 class="font-semibold text-20 ml-[300px]">Hello, </h1>
-                <a href="" class="font-semibold text-20 ml-[50px]">Log Out</a>
+                <a href="Cu-logout.php" class="font-semibold text-20 ml-[50px]">Log Out</a>
             </div>
             <div class="flex mt-2 ml-16 justify-start flex-wrap gap-8">
+                <?php
+                while($row = mysqli_fetch_assoc($result)){
+                ?>
                     <div class="w-1/6 border duration-700 ease-in-out rounded-lg shadow bg-navy hover:bg-li-navy">
                         <div class="flex flex-col items-center py-5">
                             <img class="block w-36 h-36 mb-3 shadow-lg" src="./images/nivialotion.png"
                                 alt="Bonnie image" />
-                            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white"></h5>
-                            
-                            <form class="ml-10" method = "POST" action="">
+                            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white"><?php echo $row['medicine_name'] ?></h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400"><?php echo $row['dosage'] ?>mgf</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400"><?php echo $row['type'] ?></span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Price : <?php echo $row['sell_price'] ?></span>
+                            <form class="ml-10" method = "POST" action="cu-add_cart.php?name=<?php echo $row['medicine_name']; ?>">
                                 <input type="text" class="w-2/3 rounded-lg text-center form-control" placeholder="  Quantity" name = "qu">
                                 <input type = "submit" class = "btn btn-lg btn-primary inline-flex items-center px-2 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-2" value = "Add to Cart">
                             </form>
                         </div>
                     </div>
+                <?php } ?>
             </div>
         </div>
 </body>
